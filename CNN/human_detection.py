ee7feb2287935216f4e43faa5f046b7854d0bac4
Luca Cimino <luca.cimino01@icloud.com>
2024-01-27 15:29:57 +0100

    Rete funzionante

--- CNN/human_detection.py
+++ CNN/human_detection.py
@@ -3,10 +3,17 @@ from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
+########################
+# IMAGE SIZE
+WEIGHT = 256
+HEIGHT = 256
+########################
+
+
 model = models.Sequential()
 
 # Dimensioni delle immagini in ingresso alla nostra rete
-input_shape=(64, 64, 3)
+input_shape=(WEIGHT, HEIGHT, 3)
 
 model.add(layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=input_shape))
 model.add(layers.MaxPooling2D((2, 2)))
@@ -31,23 +38,23 @@ model.summary()
 
 
 # Percorsi dei dataset
-train_data_dir = "/home/luca/Scrivania/Magistrale/SistemiDigitali/dataset/training"
-test_data_dir = "/home/luca/Scrivania/Magistrale/SistemiDigitali/dataset/testing"
+train_data_dir = "/home/luca/Scrivania/Magistrale/SistemiDigitali/dataset/training/"
+test_data_dir = "/home/luca/Scrivania/Magistrale/SistemiDigitali/dataset/testing/"
 
 train_datagen = ImageDataGenerator(rescale=1.0/255)
 test_datagen = ImageDataGenerator(rescale=1.0/255)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
-    target_size=(64, 64),
-    batch_size=32,
+    target_size=(WEIGHT, HEIGHT),
+    batch_size=16,
     class_mode='binary'
 )
 
 test_generator = test_datagen.flow_from_directory(
     test_data_dir,
-    target_size=(64, 64),
-    batch_size=32,
+    target_size=(WEIGHT, HEIGHT),
+    batch_size=16,
     class_mode='binary'
 )
 
--- CNN/predict.py
+++ CNN/predict.py
@@ -6,15 +6,12 @@ import numpy as np
 import sys
 
 
+########################
+# IMAGE SIZE
+WEIGHT = 256
+HEIGHT = 256
+########################
 
-CATEGORIES = ["People", "No people"]
-
-
-def prepare(filepath):
-    IMG_SIZE = 64
-    img_array = cv2.imread(filepath)
-    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-    return new_array
 
 if len(sys.argv) != 2:
     print("Usage: python3 predict.py PATH_IMMAGINE")
@@ -24,21 +21,29 @@ path_img = sys.argv[1]
 
 model = models.load_model('model')
 
-# scaliamo i valori
-img = prepare(path_img)
+img_array = cv2.imread(path_img)
+new_array = cv2.resize(img_array, (WEIGHT, HEIGHT))
 
-x = image.img_to_array(img)
-x /= 255 
-x = np.expand_dims(x, axis=0)
+# scaliamo i valori
+img = image.img_to_array(new_array)
+img /= 255 
+img = np.expand_dims(img, axis=0)
+
 # appiattiamo l'output
-images = np.vstack([x])
+images = np.vstack([img])
   
 # eseguiamo la predizione
-classes = model.predict(images, batch_size=10)
+predictions = model.predict(images, batch_size=10, verbose=2)
   
-print(classes[0])
-  
-if classes[0] > 0.5:
+print(predictions[0])
+
+#####################
+# classes[0]  =>  % People
+# classes[1]  =>  % No People
+#####################
+
+if predictions[0] > 0.5:
     print("People!")
 else:
     print("No people!")
+  
