c9d991531f09f77f8320a5c580ce057aa877fc0c
stefano <stefano.spadari@studio.unibo.com>
2024-02-06 18:35:34 +0100

    Reformat file clientSocketUDP_YUV.py

--- app/clientSocketUDP_YUV.py
+++ app/clientSocketUDP_YUV.py
@@ -4,12 +4,12 @@ import sys
 import numpy as np
 
 if len(sys.argv) != 3:
-        print("usage: serverSocket ip_server port_server")
-        exit()
+    print("usage: serverSocket ip_server port_server")
+    exit()
 
-#indirizzo ip e porta del server
-server_ip= sys.argv[1]
-server_port=int(sys.argv[2])
+# indirizzo ip e porta del server
+server_ip = sys.argv[1]
+server_port = int(sys.argv[2])
 
 # Creazione della socket UDP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -39,7 +39,7 @@ while True:
         print("Timeout sulla ricezione della conferma. Riprovare.")
 
 # Configurazione della finestra per la visualizzazione dei frame
-#cv2.namedWindow("Received Frame", cv2.WINDOW_NORMAL)
+# cv2.namedWindow("Received Frame", cv2.WINDOW_NORMAL)
 
 while True:
     try:
@@ -50,9 +50,9 @@ while True:
 
         # Conversione YUV -> RGB
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_I420)
-        print('dimensione prima del resize',frame_rgb.shape)
+        print('dimensione prima del resize', frame_rgb.shape)
         frame_rgb = cv2.resize(frame_rgb, (640, 480))
-        print('dimensione dopo il resize',frame_rgb.shape)
+        print('dimensione dopo il resize', frame_rgb.shape)
 
         # Visualizzazione del frame
         cv2.imshow("Received Frame", frame_rgb)
@@ -60,17 +60,15 @@ while True:
         # Interruzione del loop se viene premuto il tasto 'q'
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
-    except socket.timeout:    
+
+    except socket.timeout:
         # Gestione del timeout
         print('timeout, stampo frame nero')
         cv2.imshow("Received Frame", black_frame)
-        cv2.waitKey(1)       
+        cv2.waitKey(1)
         continue
-        #print("Timeout sulla ricezione della conferma. Riprovare.")   
-
+        # print("Timeout sulla ricezione della conferma. Riprovare.")
 
 # Chiusura della finestra e della socket
 cv2.destroyAllWindows()
 client_socket.close()
-
